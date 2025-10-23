5fbf5a23602971baabef70bfa439353d2bb40b57
Zakariaa Sabah <zakariasabah2000@gmail.com>
2025-10-23 23:40:02 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -4,15 +4,15 @@ import java.util.Date;
 public class Test {
     public static void main(String[] args) {
 
-        Commande c1 = new Commande(101, new Date(125, 0, 5), "Fournisseur A");
-        Commande c2 = new Commande(102, new Date(124, 7, 20), "Fournisseur B");
-        Commande c3 = new Commande(103, new Date(126, 2, 10), "Fournisseur C");
+        Commande c1 = new Commande(101, new Date(125, 0, 5), "Fourn 1");
+        Commande c2 = new Commande(102, new Date(124, 7, 20), "Fourn 2");
+        Commande c3 = new Commande(103, new Date(126, 2, 10), "Fourn 3");
 
-        Client cl = new Client(1, "Ali", "Casablanca", "0600123456");
+        Client cl = new Client(1, "Zakariaa", "Casablanca", "0631368896");
         cl.EnregistrerCommande(c1);
         cl.EnregistrerCommande(c2);
 
-        ClientFidel cf = new ClientFidel(2, "Omar", "Rabat", "0700654321", 0.15);
+        ClientFidel cf = new ClientFidel(2, "Adam", "Rabat", "0652654834", 0.15);
         cf.EnregistrerCommande(c3);
         double remiseRetour = cf.getMontantRemise();
 
